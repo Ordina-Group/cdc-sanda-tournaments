31ca80cab3961cf259ee8196d407c3a5bedb29cd
madiordina <mdudaeva@hziv.be>
2019-09-26 14:39:15 +0200

    First commit

---
+++ src/main/java/be/ordina/sandatournaments/SandaTournamentsApplication.java
@@ -0,0 +1,13 @@
+package be.ordina.sandatournaments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandaTournamentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandaTournamentsApplication.class, args);
+	}
+
+}
